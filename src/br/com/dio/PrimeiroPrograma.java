db95cd034b2daec1dcdcbe4b04c469c8fd2ba8ef
Jean Pinnow <soriapinnow@gmail.com>
2021-10-27 12:26:10 -0300

    Criando classe Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,8 +4,46 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        Gato gato = new Gato();
+        Gato gato = new Gato("Felix", "preta", 4);
         System.out.println(gato);
+
+        Livro livro = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro);
+
+    }
+}
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+
 
     }
 }
